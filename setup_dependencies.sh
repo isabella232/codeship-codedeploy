efa34c7269b35d35a7c14ef0812d918da89a5bc8
Florian Motlik <flomotlik@gmail.com>
2014-11-07 11:02:05 +0100

    Output directory content and move back into clone folder

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -5,5 +5,7 @@ cd /tmp/botocore/data/aws/codedeploy/
 aws s3 cp s3://razorbill-us-east-1-prod-default-distribution/latest/2014-10-06.api.json .
 export AWS_DATA_PATH=/tmp/botocore/data
 aws s3 cp --recursive s3://razorbill-us-east-1-prod-default-distribution/latest .
+ls -lA
 pip install awscli-1.5.4.tar.gz
 aws deploy list-applications
+cd ~/clone
