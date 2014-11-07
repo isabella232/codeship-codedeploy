b981ee30ffa7d4c49c0692eb8189afa1cc7739b1
Florian Motlik <flo@codeship.io>
2014-11-07 11:28:35 +0100

    yes on awscli uninstall

--- setup_dependencies.sh
+++ setup_dependencies.sh
@@ -10,7 +10,7 @@ cd /tmp/botocore/data/aws/codedeploy/
 aws s3 cp s3://razorbill-us-east-1-prod-default-distribution/latest/2014-10-06.api.json .
 export AWS_DATA_PATH=/tmp/botocore/data
 aws s3 cp --recursive s3://razorbill-us-east-1-prod-default-distribution/latest .
-pip uninstall awscli
+pip uninstall awscli -y
 ls -lA
 pip install awscli-1.5.4.tar.gz
 aws deploy list-applications
